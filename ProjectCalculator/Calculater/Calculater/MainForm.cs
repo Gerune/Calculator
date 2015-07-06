729259711e14b325e30c0a89e96132221555a2f3
Gerune <shisutaki@gmail.com>
2015-07-06 16:58:03 +0600

    FinalRefactoring

--- ProjectCalculator/Calculater/Calculater/MainForm.cs
+++ ProjectCalculator/Calculater/Calculater/MainForm.cs
@@ -21,78 +21,32 @@ namespace Calculater
         {
             double firstArgument = ParsArgument(FirstInput.Text);
             double secondArgument = ParsArgument(SecondInput.Text);
+            double result;
             {
                 switch (((Button)sender).Name)
                 {
                     case "Summary":
-                        Value.Text = Convert.ToString(firstArgument + secondArgument);
+                        result = firstArgument + secondArgument;
                         break;
                     case "TakeAway":
-                        Value.Text = Convert.ToString(firstArgument - secondArgument);
+                        result = firstArgument - secondArgument;
                         break;
                     case "Multiplication":
-                        Value.Text = Convert.ToString(firstArgument * secondArgument);
+                        result = firstArgument * secondArgument;
                         break;
                     case "Division":
-                        if (secondArgument == 0)
+                        if (secondArgument == 0.0)
                         {
                             throw new Exception("Division on Zero");
                         }
 
-                        Value.Text = Convert.ToString(firstArgument / secondArgument);
+                        result = firstArgument / secondArgument;
                         break;
                     default: throw new Exception();
                 }
+
+                Value.Text = Convert.ToString(result);
             }
-
         }
-
-        //private void TakeAway_Click(object sender, EventArgs e)
-        //{
-        //    double firstArgument, secondArgument;
-        //    ParsData()
-        //    Value.Text = Convert.ToString(firstArgument - secondArgument);
-        //}
-
-        //private void Division_Click(object sender, EventArgs e)
-        //{
-        //    double firstArgument, secondArgument;
-        //    if (double.TryParse(FirstInput.Text, out firstArgument) &&
-        //       double.TryParse(SecondInput.Text, out secondArgument) && secondArgument != 0)
-        //    {
-        //        Value.Text = Convert.ToString(firstArgument / secondArgument);
-        //    }
-        //    else
-        //    {
-        //        Value.Text = "Error";
-        //    }
-        //}
-
-        //private void Sum_Click(object sender, EventArgs e)
-        //{
-        //    double firstArgument, secondArgument;
-        //    if (double.TryParse(FirstInput.Text, out firstArgument) &&
-        //       double.TryParse(SecondInput.Text, out secondArgument))
-        //    {
-        //        Value.Text = Convert.ToString(firstArgument + secondArgument);
-        //    }
-        //    else
-        //    {
-        //        Value.Text = "Error";
-        //    }
-        //}
-
-        //private void Multiplication_Click(object sender, EventArgs e)
-        //{
-        //    double firstArgument, secondArgument;
-        //    if (double.TryParse(FirstInput.Text, out firstArgument) &&
-        //       double.TryParse(SecondInput.Text, out secondArgument))
-        //    {
-        //        Value.Text = Convert.ToString(firstArgument * secondArgument);
-        //    }
-        //    else
-        //    {
-        //        Value.Text = "Error";
-        //    }
     }
 }
