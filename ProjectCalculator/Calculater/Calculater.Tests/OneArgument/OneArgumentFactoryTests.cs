6b118f8739d1be181cbdf10abf128556c34a2e4e
Gerune <shisutaki@gmail.com>
2015-07-09 13:09:44 +0600

    FactoryTests
    Testing Factories

--- ProjectCalculator/Calculater/Calculater.Tests/OneArgument/OneArgumentFactoryTests.cs
+++ ProjectCalculator/Calculater/Calculater.Tests/OneArgument/OneArgumentFactoryTests.cs
@@ -1,7 +1,26 @@
-﻿
-namespace Calculater.Tests.OneArgument
+﻿using System;
+using NUnit.Framework;
+
+namespace Calculater.OneArgument
 {
+    [TestFixture]
     public class OneArgumentFactoryTests
     {
+        class OneArgumetFactoryTests
+        {
+            [TestCase(typeof(Abs), "Abs")]
+            [TestCase(typeof(Cos), "Cos")]
+            [TestCase(typeof(Sin), "Sin")]
+            [TestCase(typeof(DivisionByX), "DivisionByX")]
+            [TestCase(typeof(Gradus), "Gradus")]
+            [TestCase(typeof(Square), "Square")]
+            [TestCase(typeof(Tg), "Tg")]
+            [TestCase(typeof(Sqrt), "Sqrt")]
+            public void CreateCalculator(Type expectedTypeOfOperation, string nameOfOperation)
+            {
+                Type resultType = OneArgumentFactory.CreateCalculator(nameOfOperation).GetType();
+                Assert.AreEqual(expectedTypeOfOperation, resultType);
+            }
+        }
     }
 }
--- ProjectCalculator/Calculater/Calculater.Tests/TwoArguments/TwoArgumentsFactoryTests.cs
+++ ProjectCalculator/Calculater/Calculater.Tests/TwoArguments/TwoArgumentsFactoryTests.cs
@@ -1,7 +1,24 @@
-﻿namespace Calculater.Tests.TwoArguments
+﻿using System;
+using NUnit.Framework;
+
+namespace Calculater.TwoArguments
 {
     public class TwoArgumentsFactoryTests
     {
-         
+        [TestFixture]
+        class TwoArgumetFactoryTests
+        {
+            [TestCase(typeof(Division), "Division")]
+            [TestCase(typeof(Percent), "Percent")]
+            [TestCase(typeof(PowXY), "PowXY")]
+            [TestCase(typeof(Summary), "Summary")]
+            [TestCase(typeof(TakeAway), "TakeAway")]
+            [TestCase(typeof(Multiplication), "Multiplication")]
+            public void CreateCalculator(Type expectedTypeOfOperation, string nameOfOperation)
+            {
+                Type resultType = TwoArgumentFactory.CreateCalculator(nameOfOperation).GetType();
+                Assert.AreEqual(expectedTypeOfOperation, resultType);
+            }
+        }
     }
 }
